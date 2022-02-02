54368cbc089d2f060178d0aedea26e69aa6ad421
tiernan7 <tiernan7@cs.washington.edu>
2022-02-01 18:05:58 -0800

    Implemented DFS with dynamic programing for highest weight path on an arbitrary WDAG

--- Homework4/main.cpp
+++ Homework4/main.cpp
@@ -17,11 +17,11 @@ using namespace std;
 Vertex::Vertex(int lab, int sOrE){
     assert(to_string(lab).length() <= 10);
     assert(sOrE == 0 or sOrE == 1 or sOrE == -1);
-    label = lab;
-    startOrEnd = sOrE;
+    label = lab; //An integer label for the vertex
+    startOrEnd = sOrE; //-1 when an end, 0 when neither, 1 when a start
 }
     
-bool Vertex::isStart(){
+bool Vertex::isStart(){ //Returns true if the vertex is a start vertex
     if (startOrEnd == 1){
         return true;
     } 
@@ -30,7 +30,7 @@ bool Vertex::isStart(){
     }
 }
    
-bool Vertex::isEnd(){
+bool Vertex::isEnd(){//Return true if the vertex is an end vertex
     if (startOrEnd == -1){
         return true;
     }
@@ -39,23 +39,45 @@ bool Vertex::isEnd(){
     }
 }
 
-int Vertex::getNext(){
-      return next;
+void Vertex::addChild(int label,double weight){//Adds an edge directed at a child with a specified weight
+        struct Edge newEdge;
+        newEdge.otherEnd = label;
+        newEdge.weight = weight;
+        children.push_back(newEdge);
 }
 
-void Vertex::setNext(int label){
-        next = label;
-}
-
-int Vertex::getLabel(){
+int Vertex::getLabel(){ //Returns the label of the vertex as an int
         return label;
 }
 
-void Vertex::addParent(int label){
-    parents.push_back(label);
+void Vertex::addParent(int label, double weight){//Adds an edge directed from a parent to the current node with a specified weight
+    struct Edge newEdge;
+    newEdge.otherEnd = label;
+    newEdge.weight = weight;
+    parents.push_back(newEdge);
+}
+
+vector<Edge> Vertex::getParents(){//Returns the vector of incoming edges
+    return parents;
 }
     
-bool Graph::multipleStarts(){
+int Vertex::getHighestParent(){//Returns the label of the incoming path with highest weight
+    return highestParent;
+}
+
+void Vertex::setHighestParent(int highest){//Sets the value of the incoming path with the highest weight (or the current node)
+    highestParent = highest;
+}
+
+double Vertex::getHighestWeight(){//Returns the value of the weight of the highest incoming edge, or 0
+    return highestWeight;
+}
+
+void Vertex::setHighestWeight(double weight){//Sets the value of the highest weight
+    highestWeight = weight;
+}
+
+bool Graph::multipleStarts(){//Returns true if multiple start sites exist in the graph
     int numberOfStarts = 0;
     for (int i = 0; i < nodeList->size(); i++){
         if (nodeList->at(i)->isStart()){
@@ -64,36 +86,38 @@ bool Graph::multipleStarts(){
     }
     return (numberOfStarts>1);
 }
+
+
     
-bool Graph::multipleEnds(){
+bool Graph::multipleEnds(){//Returns true if multiple end sits exist in the graph
     int numberOfEnds = 0;
     for (int i = 0; i < nodeList->size(); i++){
         if (nodeList->at(i)->isEnd()){
             numberOfEnds++;
         }
-    return (numberOfEnds > 1);
     }
+    
+    return (numberOfEnds > 1);
 }
 
-bool Graph::isValidGraph(){
+bool Graph::isValidGraph(){//Checks to make sure there are not multiple starts or ends
     bool returnValue = false;
     returnValue = not(multipleStarts() or multipleEnds());
     return returnValue;
 }
 
-void Graph::processLine(string line){
+void Graph::processLine(string line){//Creates a node or edge from a given line of a graph input file
     
-    boost::char_separator<char> sep(" ");
+    boost::char_separator<char> sep(" "); //tokenizer to break each line into words
     boost::tokenizer<boost::char_separator<char> > tokens(line, sep);
     vector<string> words;
 
     for (const auto &t: tokens){
         words.push_back(t);
     } 
-    
 
     int startOrEnd = 0;
-    if (words[0] == "V"){
+    if (words[0] == "V"){//Handles the case where the line is a vertex
 
         stringstream l(words[1]);
         int label;
@@ -110,19 +134,25 @@ void Graph::processLine(string line){
 
 
         Vertex * v = new Vertex(label, startOrEnd);
-        
+        //creates the vertex and adds it to the nodes list
         nodeList->push_back(v);
+        nodeList->back()->addParent(-1,0.0);// This line adds a node as its own parent, with zero weight and -1 as the label
         }
         
-    else if (words[0] == "E"){
+    else if (words[0] == "E"){//Handles the case where the line is an edge
         stringstream start(words[2]);
         stringstream end(words[3]);
+        stringstream w(words[4]);
+        double wt;
+        w >> wt;
         int s = 0;
         int e = 0;
         start >> s;
         end >> e;
-        (*nodeList)[int(s)]->setNext(s);
-        (*nodeList)[int(e)]->addParent(s);    
+        (*nodeList)[int(s)]->addChild(s,wt);
+        (*nodeList)[int(e)]->addParent(s,wt);
+
+        
      }
 
     else {
@@ -130,11 +160,11 @@ void Graph::processLine(string line){
     }
 }
 
-int Graph::countNodes(){
+int Graph::countNodes(){ //Returns the number of nodes 
     return nodeList->size();
 }
 
-Graph::Graph(string fileName){
+Graph::Graph(string fileName){//Initializes a graph from a file
     ifstream graphFile;
     graphFile.open(fileName);
 
@@ -151,8 +181,73 @@ Graph::Graph(string fileName){
 }
 
 
+//A function that returns the highest weight path on a graph as a vector of labels
+vector<int> * Graph::getHighestPath(int start, int end){
+    vector<int> * returnPath = new vector<int>; //the longest path to be returned
+    if (start == -999 and end == -999){ //case with no constraints
+
+        for (auto &v : *nodeList){ //for each node in the graph
+
+            if (v->getParents().size() == 0){ //if depth i s0...
+
+                v->setHighestParent(-1); //highest parent is undefined
+                v->setHighestWeight(0.0); //highest weight must be 0
+
+            }
+            else{ //else we must have visited the parents, so...
+                double highestWeight = -numeric_limits<double>::infinity(); //initialize for finding max
+                double highestLabel = -1;
+
+                for (auto &p : v->getParents()){ //for each parent of the node
+                    if (p.weight > highestWeight){ //find highest weight parent
+                        highestWeight = p.weight;
+                        highestLabel = p.otherEnd;
+                    }
+                }
+                if (highestWeight == 0){
+                        highestWeight = 0.0;
+                        highestLabel = -1;
+                }
+                else{
+                v->setHighestParent(highestLabel); 
+                v->setHighestWeight(nodeList->at(highestLabel)->getHighestWeight() + highestWeight);
+                }
+            }
+            
+        }
+    }
+    double highest = numeric_limits<double>::infinity(); //varibles to keep track of the longest path
+    int highestLabel = -9999;
+
+    for (auto &v : *nodeList){
+        if (v->getHighestWeight() > -highest){
+            highest = v->getHighestWeight();
+            highestLabel = v->getLabel();
+        }
+    }
+
+    int pos = highestLabel;
+    returnPath->push_back(pos);
+    do{
+        pos = nodeList->at(pos)->getHighestParent();
+
+        returnPath->push_back(pos);
+    } while ((*nodeList).at(pos)->getHighestParent() != -1);
+
+    reverse(returnPath->begin(), returnPath->end());
+
+
+    return returnPath;
+
+    
+}
+
+
 int main(){
-    Graph * input = new Graph("Graph.txt");
+    Graph * input = new Graph("graphCP003508.txt");
     cout << input->countNodes() << endl;
+    for (auto &i : *(input)->getHighestPath(-999,-999)){
+        cout << i << endl;
+    }
     return 0;
 }
--- Homework4/main.h
+++ Homework4/main.h
@@ -1,23 +1,39 @@
 #ifndef MAIN
 #define MAIN
 
+//A struct representing an edge
+//Each edge is stored to be stored within a vertex object
+struct Edge{
+    int otherEnd;
+    double weight;
+};
+
+//A class representing a vertex on a graph
 class Vertex{
     private:
         int label;
         int startOrEnd;
-        int next;
-        std::vector<int> parents;
+        std::vector<Edge> children;
+        std::vector<Edge> parents;
+        int highestParent;
+        double highestWeight;
 
     public:
         Vertex(int label, int sOrE);
         bool isStart();
         bool isEnd();
-        int getNext();
-        void setNext(int label);
+        void addChild(int label, double weight);
         int getLabel();
-        void addParent(int label);
+        void addParent(int label, double weight);
+        std::vector<Edge> getParents();
+        int getHighestParent();
+        void setHighestParent(int highest);
+        double getHighestWeight();
+        void setHighestWeight(double weight);
 };
 
+//A class representing an entire graph with member functions for graph search
+//  and manipulation
 class Graph{
     public:
         std::vector<Vertex*> * nodeList = new std::vector<Vertex*>;
@@ -29,8 +45,11 @@ class Graph{
         bool isValidGraph();
         void processLine(std::string line);
         int countNodes();
-
+        
         Graph(std::string fileName);
+
+        std::vector<int> * getHighestPath(int start, int end);
+
 };
 
 
